0637e2c5c38b5c178ba5bb19b5faa773566b141f
artick788 <arthur.preal@gmail.com>
2026-04-27 10:39:32 +0200

    Added extra tests for the framebuffer

--- test/Context/TestFrameBuffer.cpp
+++ test/Context/TestFrameBuffer.cpp
@@ -185,155 +185,149 @@ TYPED_TEST(TestFrameBuffer, ClearFrameBuffer1DS) {
     this->compareDepthStencilData(dsDesc.clearDepth, dsDesc.clearStencil, dsAttachment);
 }
 
-// TYPED_TEST(TestFrameBuffer, ClearFrameBuffer2CATexture) {
-//     auto fbLayout = Environment<TypeParam>::m_Window->getContext()->createFrameBufferLayout();
-//     fbLayout->setDimensions(this->m_Width, this->m_Height);
-//     FrameBufferColorAttachmentDesc ca1Desc;
-//     const Utils::Color clearColor1(1.0f, 0.0f, 1.0f, 1.0f);
-//     ca1Desc.clearColor = clearColor1;
-//     ca1Desc.format = VL_TEXTURE_RGBA_F32;
-//     ca1Desc.enableShaderAccess = true;
-//     ca1Desc.usage = VL_BUFFER_USAGE_DYNAMIC;
-//     fbLayout->addColorAttachment(ca1Desc);
-//
-//     FrameBufferColorAttachmentDesc ca2Desc;
-//     const Utils::Color clearColor2(0.0f, 1.0f, 1.0f, 1.0f);
-//     ca2Desc.clearColor = clearColor2;
-//     ca2Desc.format = VL_TEXTURE_RGBA_U8; // Intentionally use a different format to test multiple attachments with different formats
-//     ca2Desc.enableShaderAccess = true;
-//     ca2Desc.usage = VL_BUFFER_USAGE_DYNAMIC;
-//     fbLayout->addColorAttachment(ca2Desc);
-//
-//     // Create the Frame Buffer
-//     auto fb = Environment<TypeParam>::m_Window->getContext()->createFrameBuffer(fbLayout);
-//     ASSERT_NE(fb, nullptr);
-//
-//     // Clear it
-//     fb->clear();
-//
-//     // Check if the clear colors are correct
-//     const auto& ca1 = fb->getColorAttachment(0);
-//     ASSERT_NE(ca1, nullptr);
-//     this->compareColorAttachmentDataF32(clearColor1, ca1);
-//
-//     const auto& ca2 = fb->getColorAttachment(1);
-//     ASSERT_NE(ca2, nullptr);
-//     const std::array<U8, 4> clearColor2U8 = {
-//         static_cast<U8>(clearColor2.r * 255),
-//         static_cast<U8>(clearColor2.g * 255),
-//         static_cast<U8>(clearColor2.b * 255),
-//         static_cast<U8>(clearColor2.a * 255)
-//     };
-//     this->compareColorAttachmentDataU8(clearColor2U8, ca2);
-// }
-//
-// TYPED_TEST(TestFrameBuffer, DrawFrameBuffer1CATexture) {
-//     // Create the screen shader
-//     const ShaderPackage screenShader = Environment<TypeParam>::createShader(Test::getScreenVertexShader1CA<TypeParam::value>(), Test::getScreenFragmentShader1CA<TypeParam::value>());
-//
-//     // Create a simple screen rectangle
-//     auto vLayout = Environment<TypeParam>::m_Window->getContext()->createVertexLayout();
-//     vLayout->addAttribute("Position", VL_FLOAT32_3);
-//     vLayout->addAttribute("Color", VL_FLOAT32_3);
-//
-//     VertexBufferDesc vbDesc;
-//     vbDesc.count = 4;
-//     vbDesc.data = Test::s_ScreenVertices.data();
-//     vbDesc.layout = vLayout;
-//
-//     IndexBufferDesc ibDesc;
-//     ibDesc.count = 6;
-//     ibDesc.data = Test::s_RectangleIndices.data();
-//     ibDesc.dataType = VL_UINT32;
-//
-//     MeshBindingDesc mbDesc;
-//     mbDesc.vertexBuffer = Environment<TypeParam>::m_Window->getContext()->createVertexBuffer(vbDesc);
-//     mbDesc.indexBuffer = Environment<TypeParam>::m_Window->getContext()->createIndexBuffer(ibDesc);
-//     auto screenQuad = Environment<TypeParam>::m_Window->getContext()->createMeshBinding(mbDesc);
-//
-//     // Now create the 1CATexture Frame Buffer
-//     auto fbLayout = Environment<TypeParam>::m_Window->getContext()->createFrameBufferLayout();
-//     fbLayout->setDimensions(this->m_Width, this->m_Height);
-//     FrameBufferColorAttachmentDesc caDesc;
-//     const Utils::Color clearColor(0.0f, 1.0f, 0.0f, 1.0f);
-//     caDesc.clearColor = clearColor;
-//     caDesc.format = VL_TEXTURE_RGBA_F32;
-//     caDesc.enableShaderAccess = true;
-//     caDesc.usage = VL_BUFFER_USAGE_DYNAMIC;
-//     fbLayout->addColorAttachment(caDesc);
-//
-//     auto fb = Environment<TypeParam>::m_Window->getContext()->createFrameBuffer(fbLayout);
-//     ASSERT_NE(fb, nullptr);
-//
-//     // Draw to the Frame Buffer
-//     fb->begin();
-//     screenShader.shader->bind();
-//     screenQuad->draw();
-//     fb->end();
-//
-//     // Check if the drawn color is correct
-//     const Utils::Color expectedColor(1.0f, 1.0f, 0.0f, 1.0f); // The shader outputs yellow
-//     const auto& ca1 = fb->getColorAttachment(0);
-//     this->compareColorAttachmentDataF32(expectedColor, ca1);
-// }
-//
-// TYPED_TEST(TestFrameBuffer, DrawFrameBuffer2CATexture) {
-//     // Create the screen shader
-//     const ShaderPackage screenShader = Environment<TypeParam>::createShader(Test::getScreenVertexShader2CA<TypeParam::value>(), Test::getScreenFragmentShader2CA<TypeParam::value>());
-//
-//     // Create a simple screen rectangle
-//     auto vLayout = Environment<TypeParam>::m_Window->getContext()->createVertexLayout();
-//     vLayout->addAttribute("Position", VL_FLOAT32_3);
-//     vLayout->addAttribute("Color1", VL_FLOAT32_3);
-//     vLayout->addAttribute("Color2", VL_FLOAT32_3);
-//
-//     VertexBufferDesc vbDesc;
-//     vbDesc.count = 4;
-//     vbDesc.data = Test::s_ScreenVertices2.data();
-//     vbDesc.layout = vLayout;
-//
-//     IndexBufferDesc ibDesc;
-//     ibDesc.count = 6;
-//     ibDesc.data = Test::s_RectangleIndices.data();
-//     ibDesc.dataType = VL_UINT32;
-//
-//     MeshBindingDesc mbDesc;
-//     mbDesc.vertexBuffer = Environment<TypeParam>::m_Window->getContext()->createVertexBuffer(vbDesc);
-//     mbDesc.indexBuffer = Environment<TypeParam>::m_Window->getContext()->createIndexBuffer(ibDesc);
-//     auto screenQuad = Environment<TypeParam>::m_Window->getContext()->createMeshBinding(mbDesc);
-//
-//     // Now create the 1CATexture Frame Buffer
-//     auto fbLayout = Environment<TypeParam>::m_Window->getContext()->createFrameBufferLayout();
-//     fbLayout->setDimensions(this->m_Width, this->m_Height);
-//     FrameBufferColorAttachmentDesc caDesc;
-//     const Utils::Color clearColor(0.0f, 1.0f, 0.0f, 1.0f);
-//     caDesc.clearColor = clearColor;
-//     caDesc.format = VL_TEXTURE_RGBA_F32;
-//     caDesc.enableShaderAccess = true;
-//     caDesc.usage = VL_BUFFER_USAGE_DYNAMIC;
-//     fbLayout->addColorAttachment(caDesc);
-//     FrameBufferColorAttachmentDesc caDescRb;
-//     caDescRb.clearColor = clearColor;
-//     caDescRb.format = VL_TEXTURE_RGBA_F32;
-//     caDescRb.enableShaderAccess = false; // This will force the implementation to use a renderbuffer for this attachment
-//     caDescRb.usage = VL_BUFFER_USAGE_DYNAMIC;
-//     fbLayout->addColorAttachment(caDescRb); // Add a second attachment with the same properties to test multiple attachments
-//
-//     auto fb = Environment<TypeParam>::m_Window->getContext()->createFrameBuffer(fbLayout);
-//     ASSERT_NE(fb, nullptr);
-//
-//     // Draw to the Frame Buffer
-//     fb->begin();
-//     screenShader.shader->bind();
-//     screenQuad->draw();
-//     fb->end();
-//
-//     // Check if the drawn color is correct
-//     const Utils::Color expectedColor1(1.0f, 1.0f, 0.0f, 1.0f); // The shader outputs yellow
-//     const auto& ca1 = fb->getColorAttachment(0);
-//     this->compareColorAttachmentDataF32(expectedColor1, ca1);
-//
-//     const Utils::Color expectedColor2(0.0f, 0.0f, 1.0f, 1.0f); // The shader outputs blue
-//     const auto& ca2 = fb->getColorAttachment(1);
-//     this->compareColorAttachmentDataF32(expectedColor2, ca2);
-// }
+TYPED_TEST(TestFrameBuffer, ClearFrameBuffer2CA) {
+    auto fbLayout = Environment<typename TestFixture::WRAPPER>::m_Window->getContext()->createFrameBufferLayout();
+    fbLayout->setDimensions(this->m_Width, this->m_Height);
+    FrameBufferColorAttachmentDesc ca1Desc;
+    const Utils::Color clearColor1(1.0f, 0.0f, 1.0f, 1.0f);
+    ca1Desc.clearColor = clearColor1;
+    ca1Desc.format = this->m_CAFormat;
+    ca1Desc.enableShaderAccess = this->m_ShaderAccess;
+    ca1Desc.usage = VL_BUFFER_USAGE_DYNAMIC;
+    fbLayout->addColorAttachment(ca1Desc);
+
+    FrameBufferColorAttachmentDesc ca2Desc;
+    const Utils::Color clearColor2(0.0f, 1.0f, 1.0f, 1.0f);
+    ca2Desc.clearColor = clearColor2;
+    ca2Desc.format = this->m_CAFormat;
+    ca2Desc.enableShaderAccess = this->m_ShaderAccess;
+    ca2Desc.usage = VL_BUFFER_USAGE_DYNAMIC;
+    fbLayout->addColorAttachment(ca2Desc);
+
+    // Create the Frame Buffer
+    auto fb = Environment<typename TestFixture::WRAPPER>::m_Window->getContext()->createFrameBuffer(fbLayout);
+    ASSERT_NE(fb, nullptr);
+
+    // Clear it
+    fb->clear();
+
+    // Check if the clear colors are correct
+    const auto& ca1 = fb->getColorAttachment(0);
+    ASSERT_NE(ca1, nullptr);
+    this->compareColorAttachmentData(clearColor1, ca1);
+
+    const auto& ca2 = fb->getColorAttachment(1);
+    ASSERT_NE(ca2, nullptr);
+    this->compareColorAttachmentData(clearColor2, ca2);
+}
+
+TYPED_TEST(TestFrameBuffer, DrawFrameBuffer1CA) {
+    // Create the screen shader
+    const ShaderPackage screenShader = Environment<typename TestFixture::WRAPPER>::createShader(Test::getScreenVertexShader1CA<TypeParam::api>(), Test::getScreenFragmentShader1CA<TypeParam::api>());
+
+    // Create a simple screen rectangle
+    auto vLayout = Environment<typename TestFixture::WRAPPER>::m_Window->getContext()->createVertexLayout();
+    vLayout->addAttribute("Position", VL_FLOAT32_3);
+    vLayout->addAttribute("Color", VL_FLOAT32_3);
+
+    VertexBufferDesc vbDesc;
+    vbDesc.count = 4;
+    vbDesc.data = Test::s_ScreenVertices.data();
+    vbDesc.layout = vLayout;
+
+    IndexBufferDesc ibDesc;
+    ibDesc.count = 6;
+    ibDesc.data = Test::s_RectangleIndices.data();
+    ibDesc.dataType = VL_UINT32;
+
+    MeshBindingDesc mbDesc;
+    mbDesc.vertexBuffer = Environment<typename TestFixture::WRAPPER>::m_Window->getContext()->createVertexBuffer(vbDesc);
+    mbDesc.indexBuffer = Environment<typename TestFixture::WRAPPER>::m_Window->getContext()->createIndexBuffer(ibDesc);
+    auto screenQuad = Environment<typename TestFixture::WRAPPER>::m_Window->getContext()->createMeshBinding(mbDesc);
+
+    // Now create the 1CATexture Frame Buffer
+    auto fbLayout = Environment<typename TestFixture::WRAPPER>::m_Window->getContext()->createFrameBufferLayout();
+    fbLayout->setDimensions(this->m_Width, this->m_Height);
+    FrameBufferColorAttachmentDesc caDesc;
+    const Utils::Color clearColor(0.0f, 1.0f, 0.0f, 1.0f);
+    caDesc.clearColor = clearColor;
+    caDesc.format = this->m_CAFormat;
+    caDesc.enableShaderAccess = this->m_ShaderAccess;
+    caDesc.usage = VL_BUFFER_USAGE_DYNAMIC;
+    fbLayout->addColorAttachment(caDesc);
+
+    auto fb = Environment<typename TestFixture::WRAPPER>::m_Window->getContext()->createFrameBuffer(fbLayout);
+    ASSERT_NE(fb, nullptr);
+
+    // Draw to the Frame Buffer
+    fb->begin();
+    screenShader.shader->bind();
+    screenQuad->draw();
+    fb->end();
+
+    // Check if the drawn color is correct
+    const Utils::Color expectedColor(1.0f, 1.0f, 0.0f, 1.0f); // The shader outputs yellow
+    const auto& ca1 = fb->getColorAttachment(0);
+    this->compareColorAttachmentData(expectedColor, ca1);
+}
+
+TYPED_TEST(TestFrameBuffer, DrawFrameBuffer2CA) {
+    // Create the screen shader
+    const ShaderPackage screenShader = Environment<typename TestFixture::WRAPPER>::createShader(Test::getScreenVertexShader2CA<TypeParam::api>(), Test::getScreenFragmentShader2CA<TypeParam::api>());
+
+    // Create a simple screen rectangle
+    auto vLayout = Environment<typename TestFixture::WRAPPER>::m_Window->getContext()->createVertexLayout();
+    vLayout->addAttribute("Position", VL_FLOAT32_3);
+    vLayout->addAttribute("Color1", VL_FLOAT32_3);
+    vLayout->addAttribute("Color2", VL_FLOAT32_3);
+
+    VertexBufferDesc vbDesc;
+    vbDesc.count = 4;
+    vbDesc.data = Test::s_ScreenVertices2.data();
+    vbDesc.layout = vLayout;
+
+    IndexBufferDesc ibDesc;
+    ibDesc.count = 6;
+    ibDesc.data = Test::s_RectangleIndices.data();
+    ibDesc.dataType = VL_UINT32;
+
+    MeshBindingDesc mbDesc;
+    mbDesc.vertexBuffer = Environment<typename TestFixture::WRAPPER>::m_Window->getContext()->createVertexBuffer(vbDesc);
+    mbDesc.indexBuffer = Environment<typename TestFixture::WRAPPER>::m_Window->getContext()->createIndexBuffer(ibDesc);
+    auto screenQuad = Environment<typename TestFixture::WRAPPER>::m_Window->getContext()->createMeshBinding(mbDesc);
+
+    // Now create the 1CATexture Frame Buffer
+    auto fbLayout = Environment<typename TestFixture::WRAPPER>::m_Window->getContext()->createFrameBufferLayout();
+    fbLayout->setDimensions(this->m_Width, this->m_Height);
+    FrameBufferColorAttachmentDesc caDesc;
+    const Utils::Color clearColor(0.0f, 1.0f, 0.0f, 1.0f);
+    caDesc.clearColor = clearColor;
+    caDesc.format = this->m_CAFormat;
+    caDesc.enableShaderAccess = this->m_ShaderAccess;
+    caDesc.usage = VL_BUFFER_USAGE_DYNAMIC;
+    fbLayout->addColorAttachment(caDesc);
+    FrameBufferColorAttachmentDesc caDescRb;
+    caDescRb.clearColor = clearColor;
+    caDescRb.format = this->m_CAFormat;
+    caDescRb.enableShaderAccess = this->m_ShaderAccess;
+    caDescRb.usage = VL_BUFFER_USAGE_DYNAMIC;
+    fbLayout->addColorAttachment(caDescRb); // Add a second attachment with the same properties to test multiple attachments
+
+    auto fb = Environment<typename TestFixture::WRAPPER>::m_Window->getContext()->createFrameBuffer(fbLayout);
+    ASSERT_NE(fb, nullptr);
+
+    // Draw to the Frame Buffer
+    fb->begin();
+    screenShader.shader->bind();
+    screenQuad->draw();
+    fb->end();
+
+    // Check if the drawn color is correct
+    const Utils::Color expectedColor1(1.0f, 1.0f, 0.0f, 1.0f); // The shader outputs yellow
+    const auto& ca1 = fb->getColorAttachment(0);
+    this->compareColorAttachmentData(expectedColor1, ca1);
+
+    const Utils::Color expectedColor2(0.0f, 0.0f, 1.0f, 1.0f); // The shader outputs blue
+    const auto& ca2 = fb->getColorAttachment(1);
+    this->compareColorAttachmentData(expectedColor2, ca2);
+}
